48bcda146ae26f352091e56da809a6b3a893b60f
Safiya <146744427+safiyamak@users.noreply.github.com>
2024-01-06 15:28:04 -0500

    Adding API file

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ url = "https://chatgpt146.p.rapidapi.com/q"
 payload = { "prompt": "What is the meaning of life?" } #include the prompt here
 headers = {
 	"content-type": "application/json",
-	"X-RapidAPI-Key": "5af49fa643mshe938c5f470d795ep1043e7jsnc7b149f729ee",
+	"X-RapidAPI-Key": "",
 	"X-RapidAPI-Host": "chatgpt146.p.rapidapi.com"
 }
 
