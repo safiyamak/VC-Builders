7d5180de2591d2c998a74fc6242162c7ca526137
Safiya <146744427+safiyamak@users.noreply.github.com>
2024-01-06 18:06:27 -0500

    Completed api.py with prompt

--- api.py
+++ api.py
@@ -2,16 +2,18 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import os
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+def openAIGPTcall(problem, solution):
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key)
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
-)
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a data cleaner. The user will input an environmental or business problem and the user will input a solution using circular economy principles. This data is not cleaned. You act as a data cleaner who will remove all punctuation, grammar, and spelling errors. You will also make the data concise and easy to understand for another LLM that will be trained on this data."},
+        {"role": "user", "content": "Problem: " + problem + "; solution: " + solution}
+    ]
+    )
+    print(completion.choices[0].message)
 
-print(completion.choices[0].message)
+
