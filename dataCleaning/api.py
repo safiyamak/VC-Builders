c0e905c481d64ba373c7e1e4296d8f4b652b883c
Safiya <146744427+safiyamak@users.noreply.github.com>
2024-01-07 00:43:47 -0500

    Changed the escape character and removed some unnecessary lines for the GPT

--- dataCleaning/api.py
+++ dataCleaning/api.py
@@ -13,19 +13,19 @@ def openAIGPTcall(problem, solution):
     {
         "role": "system",
         "content": (
-            "You are a data cleaner. The user will input an environmental or business problem, and the user will input a solution using circular economy principles. "
-            "This data is not cleaned. You act as a data cleaner who will remove all punctuation, grammar, and spelling errors. "
+            "You are a data cleaner. The user will input an environmental or business problem, and the user will input a solution using circular economy principles. These are seperated by a newline character"
+            "This data is not cleaned. You act as a data cleaner who will remove all punctuation, grammar, and spelling errors. This does not include the unique character sequence #@#"
             "You will also make the data concise and easy to understand. The objective is to prepare the data for another language model that will be trained on this cleaned data. "
-            "Ensure the result is not longer than the original prompt. Do not remove new line characters. Remove 'Problem:' and 'Solution:'\n"
+            "Ensure the result is not longer than the original prompt. Check to ensure you have not deleted #@# from the original prompt. If you have, put them back."
         ),
     },
-    {"role": "user", "content": f"{problem}\n{solution}"},
+    {"role": "user", "content": f"{problem}#@#{solution}"},
     ]
 
     )
     responseString = completion.choices[0].message.content
     print(responseString)
-    response = responseString.split("\n")
+    response = responseString.split("#@#")
     print(response[0])
     print(response[1])
     return response
@@ -35,13 +35,10 @@ csv_file_path = os.getenv("CSV_FILE_PATH")
 df = pd.read_csv(csv_file_path,encoding='latin-1')
 fileLen = len(df)
 for i in range(5):
-    problem = df.loc[i, 'problem']
-    problem.replace('\n','')
-    soln = df.loc[i, 'solution']
-    soln.replace('\n','')
-    response = openAIGPTcall(problem, soln)
-    df.loc[i, 'problem'] = response[0]
-    df.loc[i,'solution'] = response[1]
+    problem = str(df.loc[i, 'problem'])
+    soln = str(df.loc[i, 'solution'])
+    print(f"{problem}\n{soln}")
+    #response = openAIGPTcall(problem, soln)
 df.to_csv(csv_file_path, index=False, encoding='utf-8', sep=',')
 print(f"File '{csv_file_path}' has been updated.")
 
